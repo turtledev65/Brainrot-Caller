7a8a1ea7f49c54d00a387acc5d70d80764cf7b90
turtledev <turtledev65@gmail.com>
2025-03-22 19:59:30 +0200

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <p className="px-4 font-bold  text-blue-500 bg-white text-3xl">
+        Hello World
+      </p>
+    </>
+  );
+}
+
+export default App;
