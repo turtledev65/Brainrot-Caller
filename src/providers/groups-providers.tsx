f8398bc065a7072f8084728b455ef203e262e459
turtledev <turtledev65@gmail.com>
2025-03-24 11:35:11 +0200

    Create an addCall function in the GroupsProvider and use the correct deps for useCallback

--- src/providers/groups-providers.tsx
+++ src/providers/groups-providers.tsx
@@ -9,6 +9,7 @@ type GroupsContextType = {
   addGroup: (name: string) => void;
   selectedCall: Caller | null;
   selectCall: (call: Caller | null) => void;
+  addCall: (groupName: string, call: Caller) => void;
 };
 export const GroupsContext = createContext<GroupsContextType>(
   {} as GroupsContextType,
@@ -19,25 +20,56 @@ const GroupsProvider = ({ children }: PropsWithChildren) => {
   const [selectedCall, setSelectedCall] = useState<Caller | null>(null);
   const [selectedGroup, setSelectedGroup] = useState<Group | null>(null);
 
-  const selectCall = useCallback((call: Caller | null) => {
-    setSelectedCall(call);
-  }, []);
+  const selectCall = useCallback(
+    (call: Caller | null) => {
+      setSelectedCall(call);
+    },
+    [setSelectedCall],
+  );
 
-  const selectGroup = useCallback((group: Group | null) => {
-    setSelectedGroup(group);
-  }, []);
+  const addCall = useCallback(
+    (groupName: string, call: Caller) => {
+      setGroups(prevGroups => {
+        const out = [...prevGroups];
 
-  const addGroup = useCallback((name: string) => {
-    setGroups(prevGroups => {
-      for (const group of prevGroups) {
-        if (group.name === name) {
-          return prevGroups;
+        outer: for (let i = 0; i < out.length; i++) {
+          if (out[i].name === groupName) {
+            for (const c of out[i].callers) {
+              if (call.name === c.name) break outer;
+            }
+
+            out[i].callers.push(call);
+            break;
+          }
         }
-      }
 
-      return [...prevGroups, { name, callers: [] }];
-    });
-  }, []);
+        return out;
+      });
+    },
+    [setGroups],
+  );
+
+  const selectGroup = useCallback(
+    (group: Group | null) => {
+      setSelectedGroup(group);
+    },
+    [setSelectedCall],
+  );
+
+  const addGroup = useCallback(
+    (name: string) => {
+      setGroups(prevGroups => {
+        for (const group of prevGroups) {
+          if (group.name === name) {
+            return prevGroups;
+          }
+        }
+
+        return [...prevGroups, { name, callers: [] }];
+      });
+    },
+    [setGroups],
+  );
 
   return (
     <GroupsContext.Provider
@@ -48,6 +80,7 @@ const GroupsProvider = ({ children }: PropsWithChildren) => {
         addGroup,
         selectedCall,
         selectCall,
+        addCall,
       }}
     >
       {children}
