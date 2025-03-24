f12ae2cc6ea93b6a08acaa04b615008d0eebd718
turtledev <turtledev65@gmail.com>
2025-03-24 10:39:40 +0200

    Add selectedCall to the groups provider

--- src/providers/groups-providers.tsx
+++ src/providers/groups-providers.tsx
@@ -1,5 +1,5 @@
 import { createContext, PropsWithChildren, useCallback, useState } from "react";
-import { Group } from "../types";
+import { Caller, Group } from "../types";
 import defaultGroups from "../data/default-groups";
 
 type GroupsContextType = {
@@ -7,6 +7,8 @@ type GroupsContextType = {
   selectedGroup: Group | null;
   selectGroup: (group: Group | null) => void;
   addGroup: (name: string) => void;
+  selectedCall: Caller | null;
+  selectCall: (call: Caller | null) => void;
 };
 export const GroupsContext = createContext<GroupsContextType>(
   {} as GroupsContextType,
@@ -14,19 +16,31 @@ export const GroupsContext = createContext<GroupsContextType>(
 
 const GroupsProvider = ({ children }: PropsWithChildren) => {
   const [groups, setGroups] = useState<Group[]>(defaultGroups);
+  const [selectedCall, setSelectedCall] = useState<Caller | null>(null);
   const [selectedGroup, setSelectedGroup] = useState<Group | null>(null);
+
+  const selectCall = useCallback((call: Caller | null) => {
+    setSelectedCall(call);
+  }, []);
 
   const selectGroup = useCallback((group: Group | null) => {
     setSelectedGroup(group);
   }, []);
 
   const addGroup = useCallback((name: string) => {
-    setGroups((prevGroups) => [...prevGroups, { name, callers: [] }]);
+    setGroups(prevGroups => [...prevGroups, { name, callers: [] }]);
   }, []);
 
   return (
     <GroupsContext.Provider
-      value={{ groups, selectedGroup, selectGroup, addGroup }}
+      value={{
+        groups,
+        selectedGroup,
+        selectGroup,
+        addGroup,
+        selectedCall,
+        selectCall,
+      }}
     >
       {children}
     </GroupsContext.Provider>
