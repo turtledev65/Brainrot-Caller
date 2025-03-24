548aa66462a255453584cf0f95c7d7528b501fa1
turtledev <turtledev65@gmail.com>
2025-03-24 10:40:03 +0200

    Add a call screen that displays the selected call

--- src/App.tsx
+++ src/App.tsx
@@ -3,26 +3,34 @@ import Navbar from "./components/navbar";
 import Providers from "./providers";
 import { GroupsContext } from "./providers/groups-providers";
 import { Item, ItemAdd } from "./components/item";
+import Call from "./components/call";
 
 function App() {
   return (
     <Providers>
       <Navbar />
       <GroupGrid />
+      <CallScreen />
     </Providers>
   );
 }
 
 const GroupGrid = () => {
-  const { groups, selectedGroup, selectGroup, addGroup } =
+  const { groups, selectedGroup, selectGroup, addGroup, selectCall } =
     useContext(GroupsContext);
 
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4">
       {selectedGroup !== null ? (
-        selectedGroup.callers.map(caller => (
-          <Item name={caller.name} key={caller.name} />
-        ))
+        <>
+          {selectedGroup.callers.map(caller => (
+            <Item
+              onClick={() => selectCall(caller)}
+              name={caller.name}
+              key={caller.name}
+            />
+          ))}
+        </>
       ) : (
         <>
           {groups.map(group => (
@@ -40,6 +48,21 @@ const GroupGrid = () => {
         </>
       )}
     </div>
+  );
+};
+
+const CallScreen = () => {
+  const { selectedCall, selectCall } = useContext(GroupsContext);
+
+  if (selectedCall === null) return;
+  return (
+    <Call
+      isCalling
+      onRejectCall={() => selectCall(null)}
+      name={selectedCall.name}
+      icon={selectedCall.icon}
+      background={selectedCall.background}
+    />
   );
 };
 
