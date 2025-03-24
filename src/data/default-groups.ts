ac81863a475437bb135c59e1823a0f1f8776c7c0
turtledev <turtledev65@gmail.com>
2025-03-24 12:43:06 +0200

    Add more things to the default groups

--- src/data/default-groups.ts
+++ src/data/default-groups.ts
@@ -1,7 +1,58 @@
 import { Group } from "../types";
 
 export default [
-  { name: "Popular Memes", callers: [{ name: "Test" }] },
-  { name: "Italian Brainrot", callers: [] },
+  {
+    name: "Brainrot",
+    callers: [
+      {
+        name: "Bombardiro Crocodilo",
+        icon: "https://i1.sndcdn.com/artworks-4fPGk176hMQgdghK-zFMj5Q-t500x500.jpg",
+        background:
+          "https://i1.sndcdn.com/artworks-beGlg1tvyGqU-0-t1080x1080.jpg",
+        audio:
+          "https://8bsi9qgtt2.ufs.sh/f/bFzphy5Qa8lCZPTc9S0gfvSVxkb1aLUuH5dAQKN6WTrRpn38",
+      },
+      {
+        name: "Frigo Camello",
+        icon: "https://cdn-images.dzcdn.net/images/cover/7a45297e289ff45bae9142bd69c5eb6b/1900x1900-000000-80-0-0.jpg",
+        background:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWvXEeSJe_NG73yb7W6uEQvAbw4LAuH1amBA&s",
+        audio:
+          "https://8bsi9qgtt2.ufs.sh/f/bFzphy5Qa8lCZWV5NY0gfvSVxkb1aLUuH5dAQKN6WTrRpn38",
+      },
+      {
+        name: "Cappuccino Assassino",
+        icon: "https://i.ytimg.com/vi/sy9PnLWa9jw/hq720_2.jpg?sqp=-oaymwE7CK4FEIIDSFryq4qpAy0IARUAAAAAGAAlAADIQj0AgKJD8AEB-AG2CIACuAiKAgwIABABGH8gKSgpMA8=&rs=AOn4CLABgw6QCWj90UCdq0vDgWZPVfrKHA",
+        background:
+          "https://cdn.steamstatic.com/steamcommunity/public/images/items/550900/ac4ccf572f953745c05ba6f1ba93c1d81fa4c497.jpg",
+        audio:
+          "https://8bsi9qgtt2.ufs.sh/f/bFzphy5Qa8lC0suTypO5KhJcSBQg8irCbwI3AP9OXnaj4Ge6",
+      },
+      {
+        name: "Brr Brr Patapim",
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlNyzsczj3T3ochEgVEN11RlbruydACmrzYw&s",
+        background:
+          "https://i.ytimg.com/vi/paiuv857bM4/oar2.jpg?sqp=-oaymwEiCNAFENAFSFqQAgHyq4qpAxEIARUAAAAAJQAAyEI9AICiQw==&rs=AOn4CLDGja_Q7PKdHPaOYYxgKLYADpi_zw",
+        audio:
+          "https://8bsi9qgtt2.ufs.sh/f/bFzphy5Qa8lCtUOzhHLcoSCq8lVW2FPHu7g1wnph3byzIY6J",
+      },
+      {
+        name: "Tung Tung Sahur",
+        icon: "https://i1.sndcdn.com/artworks-qJ5IFyKat8H70Vkz-tYUbnQ-t500x500.jpg",
+        background:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9BOAM0rSziVv_NrC_qvio49nYVZmMo2_UNg&s",
+        audio:
+          "https://8bsi9qgtt2.ufs.sh/f/bFzphy5Qa8lCK6J3CP1q2tFMQN7TH9BiloOuRyUVaXd1x3Ip",
+      },
+      {
+        name: "Bobritto Bandito",
+        icon: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf3kpwcuJqfaaOlOGzPuPjFWuSSn7VgG-XqA&s",
+        background:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGNvzkszEIA2cFQms_vckyt_TN12Y9GJr1HA&s",
+        audio:
+          "https://8bsi9qgtt2.ufs.sh/f/bFzphy5Qa8lCOFsG7pdLitQwgncVSyFz5AeT8ojZP1u7l2M0",
+      },
+    ],
+  },
   { name: "Custom", callers: [] },
 ] as Group[];
