65e7528a4b36be09068767cc34d9f807d78f111b
turtledev <turtledev65@gmail.com>
2025-03-24 11:36:52 +0200

    Create and AddCallItem and use correct deps for useCallback

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 }
 
 const GroupGrid = () => {
-  const { groups, selectedGroup, selectGroup, addGroup, selectCall } =
+  const { groups, selectedGroup, selectGroup, selectCall } =
     useContext(GroupsContext);
 
   return (
@@ -38,6 +38,7 @@ const GroupGrid = () => {
               key={caller.name}
             />
           ))}
+          <AddCallItem />
         </>
       ) : (
         <>
@@ -55,21 +56,85 @@ const GroupGrid = () => {
   );
 };
 
+const AddCallItem = () => {
+  const { selectedGroup, addCall } = useContext(GroupsContext);
+  const [isModalActive, setModalActive] = useState(false);
+  const nameInputRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmitForm = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!selectedGroup) return;
+      if (!nameInputRef.current) return;
+
+      const name = nameInputRef.current.value.trim();
+      if (!name || name.length === 0) return;
+
+      addCall(selectedGroup.name, { name });
+      setModalActive(false);
+      nameInputRef.current.value = "";
+    },
+    [selectedGroup, addCall, setModalActive],
+  );
+
+  return (
+    <>
+      <Modal isActive={isModalActive}>
+        <div className="flex h-full justify-center items-center">
+          <form
+            onSubmit={handleSubmitForm}
+            className="bg-white dark:bg-slate-900 rounded-sm p-4 gap-2"
+          >
+            <h2 className="text-2xl py-4 mb-4">Create Call</h2>
+            <input
+              type="text"
+              className="text-lg border-2 border-gray-400  rounded-md outline-none p-1 active:border-gray-500"
+              placeholder="Name"
+              required
+              ref={nameInputRef}
+            />
+            <div className="flex justify-evenly mt-4 text-gray-50 text-lg gap-4 py-2">
+              <button
+                type="button"
+                className="rounded-sm cursor-pointer bg-red-500 hover:bg-red-600 transition-all px-4 py-2"
+                onClick={() => setModalActive(false)}
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="rounded-sm cursor-pointer bg-green-500 hover:bg-green-600 transition-all px-4 py-2"
+              >
+                Create
+              </button>
+            </div>
+          </form>
+        </div>
+      </Modal>
+      <AddItem onClick={() => setModalActive(true)} />
+    </>
+  );
+};
+
 const AddGroupItem = () => {
   const { addGroup } = useContext(GroupsContext);
   const [isModalActive, setModalActive] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
-  const handleSubmitForm = useCallback((e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
 
-    if (!inputRef.current) return;
-    const text = inputRef.current?.value.trim();
-    if (!text || text.length == 0) return;
+  const handleSubmitForm = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!inputRef.current) return;
 
-    addGroup(text);
-    setModalActive(false);
-    inputRef.current.value = "";
-  }, []);
+      const text = inputRef.current?.value.trim();
+      if (!text || text.length == 0) return;
+
+      addGroup(text);
+      setModalActive(false);
+      inputRef.current.value = "";
+    },
+    [addGroup, setModalActive],
+  );
 
   useEffect(() => {
     if (isModalActive) inputRef.current?.focus();
@@ -84,13 +149,14 @@ const AddGroupItem = () => {
         <div className="flex h-full justify-center items-center">
           <form
             onSubmit={handleSubmitForm}
-            className="bg-gray-200 dark:bg-slate-900 rounded-sm p-4 flex flex-col items-center gap-2"
+            className="bg-white dark:bg-slate-900 rounded-sm p-4 flex flex-col items-center gap-2"
           >
             <h2 className="text-2xl py-4">Create Group</h2>
             <input
               type="text"
               className="text-lg border-2 border-gray-400  rounded-md outline-none p-1 active:border-gray-500"
               placeholder="Title"
+              required
               ref={inputRef}
             />
             <div className="flex justify-evenly text-gray-50 text-lg gap-4 py-2">
